539c897b6d46e206c56854dbe661e0373ea3096f
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2023-01-23 18:56:46 +0300

    added main project and test module

---
+++ dictionary-test-module/src/test/kotlin/com/tonyp/dictionary/FirstTest.kt
@@ -0,0 +1,12 @@
+package com.tonyp.dictionary
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FirstTest {
+
+    @Test
+    fun firstTest() {
+        assertEquals(2, 1 + 1)
+    }
+}
