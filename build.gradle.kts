29223617d605cb0d0d1eb28e81ff4ec8ebc3d344
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2023-01-23 18:56:46 +0300

    added main project and test module

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.tonyp.dictionary-kotlin"
+version = "0.1"
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    group = rootProject.group
+    version = rootProject.version
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+}
