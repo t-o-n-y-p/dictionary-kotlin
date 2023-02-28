f282c9b6b65b882cf75860cd708ec6cc6b70d83c
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2023-02-28 23:51:06 +0300

    fixed plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,13 @@ rootProject.name = "dictionary-kotlin"
 
 pluginManagement {
     val kotlinVersion: String by settings
+    val openapiVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion apply false
+
+        id("org.openapi.generator") version openapiVersion apply false
     }
 }
 include("dictionary-api-v1")
