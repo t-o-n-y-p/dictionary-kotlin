2bf3bd23c0eeeed4704a1a2e023e2555faca3f8b
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2023-03-13 22:10:37 +0300

    removed FAIL_ON_UNKNOWN_PROPERTIES setting

--- dictionary-api-v1/src/main/kotlin/ApiV1Mapper.kt
+++ dictionary-api-v1/src/main/kotlin/ApiV1Mapper.kt
@@ -1,10 +1,8 @@
 package com.tonyp.dictionarykotlin.api.v1
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 
 val apiV1Mapper = ObjectMapper().apply {
-    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     setSerializationInclusion(JsonInclude.Include.NON_NULL)
 }
